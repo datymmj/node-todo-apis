04796531fce4fd430798a070a0a00d2181ad55ce
ajaiswal29 <ajaiswal29@csc.com>
2018-05-12 16:51:15 +0530

    add POST /todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {Users} = require('./models/users'); 
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res) => {
+    // console.log(req.body);
+    var todo = new Todo({
+        text:req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    },(error) => {
+        res.status(400).send(error);
+    })
+});
+
+app.listen(3000,() => {
+    console.log('Started on port 3000');
+});
+
+
+// var newTodo = new Todo({
+//     text:'    Edit the text    '
+    // text:'Serve Dinner',
+    // completed:false,
+    // completedAt:123
+// });
+
+// newTodo.save().then((docs) => {
+// console.log('saved todo',JSON.stringify(docs,undefined,2));
+// },(error) => {
+//     console.log('Unable to save todo',error);
+// });
+
+
+
+// var newUsers = new Users({
+//     email:'akjais1994@gmail.com'
+// });
+
+// newUsers.save().then((docs) => {
+//     console.log('saved todo',JSON.stringify(docs,undefined,2));
+// },(error) => {
+//     console.log('Unable to save User',error);
+// });
