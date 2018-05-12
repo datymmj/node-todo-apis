2fb75e2827948bed32deae6408f09e33898e0d72
ajaiswal29 <ajaiswal29@csc.com>
2018-05-12 19:19:25 +0530

    Add GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,28 +1,28 @@
 const expect = require('expect');
 const request = require('supertest');
 
-const {app} = require('./../server');
-const {Todo} = require('./../models/todo');
+const { app } = require('./../server');
+const { Todo } = require('./../models/todo');
 
 beforeEach((done) => {
     Todo.remove({}).then(() => done());
 });
 
-describe('Post /todos',() => {
+describe('Post /todos', () => {
 
-    it('should create a new todo',(done) => {
-        
+    it('should create a new todo', (done) => {
+
         var text = 'text todo text';
 
         request(app)
             .post('/todos')
-            .send({text})
+            .send({ text })
             .expect(200)
             .expect((res) => {
                 expect(res.body.text).toBe(text)
             })
-            .end((err,res) => {
-                if(err){ 
+            .end((err, res) => {
+                if (err) {
                     return done(err);
                 }
 
@@ -32,24 +32,22 @@ describe('Post /todos',() => {
                     done();
                 }).catch((e) => done(e))
             });
+        });
 
-    });
-
-    it('should not create todo with invalid data',(done) => {
+    it('should not create todo with invalid data', (done) => {
         request(app)
             .post('/todos')
             .send({})
             .expect(400)
-            .end((err,res) => {
-                if(err){
+            .end((err, res) => {
+                if (err) {
                     return done(err)
                 }
+                
                 Todo.find().then((todos) => {
                     expect(todos.length).toBe(0);
                     done();
                 }).catch((e) => done(e));
-
             })
-
-    })
-});
+        })
+    });
