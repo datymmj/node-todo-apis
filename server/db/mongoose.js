eec26d754d78c5163c3315456131746ae5aa10c1
ajaiswal29 <ajaiswal29@csc.com>
2018-05-13 20:46:44 +0530

    change in the mongoose connection uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,5 +2,5 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://<firstmongodbuser>:<fmdbu123>@ds121960.mlab.com:21960/todoapi')
+mongoose.connect('mongodb://firstmongodbuser:fmdbu123@ds121960.mlab.com:21960/todoapi')
 module.exports = {mongoose};
